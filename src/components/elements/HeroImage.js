b387b3e6be92af8ed32822920048c0460cbbd2e9
github username <github email address>
2019-11-06 13:59:52 -0500

    style header

--- src/components/elements/HeroImage.js
+++ src/components/elements/HeroImage.js
@@ -1,5 +1,15 @@
 import React from "react";
+import styled from "styled-components";
 
-const HeroImage = () => <div>HeroImage</div>;
+const HeroImage = ({ image, title, text }) => (
+  <div image={image}>
+    <div className="geroimage-content">
+      <div className="heroimage-text">
+        <h1>{title}</h1>
+        <p>{text}</p>
+      </div>
+    </div>
+  </div>
+);
 
 export default HeroImage;
